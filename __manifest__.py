9989b2c7f3dd293b3ffe4810682af593db1e4e6a
zmiddd <dfendras@gmail.com>
2023-02-20 09:48:59 +0700

    ERP!

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name"          : "New Addons",
+    "version"       :"1.0",
+    "author"        :"Dimas Fendra Sumarno",
+    "website"       :"https://www.detik.com/",
+    "category"      :"New Module",
+    "summary"       :"Ini Adalah Addons Baru",
+    "description"   :"""
+        Latihan membuat addons baru di odoo 1
+    """,
+    "depends"       :[
+        "base",
+    ],
+    "data"          :[
+
+    ],
+    "demo"          :[],
+    "test"          :[],
+    "image"         :[],
+    "qweb"          :[],
+    "css"           :[],
+    "application"   :True,
+    "installable"   :True,
+    "auto_install"  :False,
+}
